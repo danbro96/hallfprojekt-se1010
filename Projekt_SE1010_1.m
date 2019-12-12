c668d90063cca37a5218a031ab189e11ddcfdacd
danbro96 <daniel.brostrom@hotmail.se>
2019-12-12 12:52:27 +0100

    Changed to one click required to run

--- Projekt_SE1010_1.m
+++ Projekt_SE1010_1.m
@@ -22,55 +22,68 @@ Grupp 15
     y = framåt
     z = uppåt
 %}
+
+
+%% 1a   -   Körning rakt fram med konstant fart, v
 clear all, close all, clc
 
 %Givna variabler
-    Projekt_SE1010_variabler
-    
-%% 1a   -   Körning rakt fram med konstant fart, v
-clc
-    lastfall = 'A';
-	v0  = v;                %m/s        Ursprungsfart
-    a   = 0;                %m/s2       Acceleration
-    t   = 0;                %s          Förfluten tid sedan påbörjad acceleration
-    
-    R0  = inf;              %m          Radie kurva
+Projekt_SE1010_variabler
+
+lastfall = 'A';
+v0  = v;                %m/s        Ursprungsfart
+a   = 0;                %m/s2       Acceleration
+t   = 0;                %s          Förfluten tid sedan påbörjad acceleration
+
+R0  = inf;              %m          Radie kurva
 
 Projekt_SE1010_jamviktsberakningar
 Projekt_SE1010_snittning
 
 %% 1b   -   Konstant acceleration, a1, påbörjad låg fart, v0
-clc
-    lastfall = 'B';
-    v0  = 1/6 * v;          %m/s        Ursprungsfart
-    a   = a1;               %m/s2       Acceleration
-    t   = 0;                %s          Förfluten tid sedan påbörjad acceleration
-    
-    R0  = inf;              %m          Radie kurva
+clear all, close all, clc
+
+%Givna variabler
+Projekt_SE1010_variabler
+
+lastfall = 'B';
+v0  = 1/6 * v;          %m/s        Ursprungsfart
+a   = a1;               %m/s2       Acceleration
+t   = 0;                %s          Förfluten tid sedan påbörjad acceleration
+
+R0  = inf;              %m          Radie kurva
 
 Projekt_SE1010_jamviktsberakningar
 %Projekt_SE1010_snittning
 
 %% 1c   -   Bromsning från maxfart med konstant retardation, a2
-clc
-    lastfall = 'C';
-    v0  = v;                %m/s        Ursprungsfart
-    a   = a2;               %m/s2       Acceleration
-    t   = 0;                %s          Förfluten tid sedan påbörjad acceleration
-    
-    R0  = inf;              %m          Radie kurva
+clear all, close all, clc
+
+%Givna variabler
+Projekt_SE1010_variabler
+
+lastfall = 'C';
+v0  = v;                %m/s        Ursprungsfart
+a   = a2;               %m/s2       Acceleration
+t   = 0;                %s          Förfluten tid sedan påbörjad acceleration
+
+R0  = inf;              %m          Radie kurva
 
 Projekt_SE1010_jamviktsberakningar
 %Projekt_SE1010_snittning
 
 %% 1d   -   Körning med konstant fart, 0.5*v, i kurva med radie R
-clc
-    lastfall = 'D';
-    v0  = 1/2 * v;          %m/s        Ursprungsfart
-    a   = 0;                %m/s2       Acceleration
-    t   = 0;                %s          Förfluten tid sedan påbörjad acceleration
-    
-    R0  = R;                %m          Radie kurva
+clear all, close all, clc
+
+%Givna variabler
+Projekt_SE1010_variabler
+
+lastfall = 'D';
+v0  = 1/2 * v;          %m/s        Ursprungsfart
+a   = 0;                %m/s2       Acceleration
+t   = 0;                %s          Förfluten tid sedan påbörjad acceleration
+
+R0  = R;                %m          Radie kurva
 
 Projekt_SE1010_jamviktsberakningar
 Projekt_SE1010_snittning
