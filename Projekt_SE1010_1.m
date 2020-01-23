097ae289b82796b210e1361ccfa832268f27d070
danbro96 <daniel.brostrom@hotmail.se>
2020-01-24 00:40:55 +0100

    Updated flange positioning plots

--- Projekt_SE1010_1.m
+++ Projekt_SE1010_1.m
@@ -41,7 +41,7 @@ Projekt_SE1010_jamviktsberakningar
 Projekt_SE1010_snittning
 
 Projekt_SE1010_plottning
-Projekt_SE1010_lokspann
+%Projekt_SE1010_lokspann
 Projekt_SE1010_utmattning
 
 %% Lastfall B   -   Konstant acceleration, a1, påbörjad låg fart, v0
@@ -135,11 +135,20 @@ for i = 0:0.01:L
 end
 %figure('Name',['Von Mises - Lastfall ' lastfall]);
 plot(0:0.01:L,VMt)
+hold on
+plot([0 L],[Ss/nu Ss/nu], 'g') 
+
+plot(b1, VMx(b1),'r*')
+plot(L/2-bb, VMx(L/2-bb),'r*')
+plot(L/2+bd, VMx(L/2+bd),'r*')
+plot(L-b1, VMx(L-b1),'r*')
+hold off
 %fplot(VM,[0 L]);
 %legend("Von Mises")
-xlabel('Position längs X-axeln [m]')
-ylabel('Moment [Nm] / Kraft [N]')
-title(['Von Mises - Lastfall ' lastfall])
+legend(['D = ' num2str(D) ' m'], 'Sträckgräns med säkerhetsfaktor för material', 'Lokala spänningskoncentrationer','Location','southoutside')
+xlabel('X-koordinat längs med bakaxeln [m]')
+ylabel('Spänning [Pa]')
+title(['Von Mises Effektivspänning - Lastfall ' lastfall])
 grid on
 hold on
 end
